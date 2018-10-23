e072877a63c765afa46b69ff628f4c2ecf0cd67c
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2018-10-23 11:34:00 +0000

    Imgur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,9 @@
         <meta property="og:title" content="Microposts〜 Twitterライクなアプリケーション" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://herokumicroposts2018.herokuapp.com/" />
-        <meta property="og:image" content="http://herokumicroposts2018.herokuapp.com/Microposts.png" />
+        <meta property="og:image" content="https://imgur.com/a/GHc3jaO" />
         <meta property="og:site_name" content="Microposts" />
         <meta property="og:description" content="フレームワークLaravelではじめてのWebサービス作りをしました。" />
-        
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
