1474bb4f213514a1a5353631e2a2aed022110864
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2018-10-23 10:18:15 +0000

    add ogp setting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Microposts</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Microposts</title>
+        <meta property="og:title" content="Microposts〜 Twitterライクなアプリケーション" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://herokumicroposts2018.herokuapp.com/" />
+        <meta property="og:image" content="http://herokumicroposts2018.herokuapp.com/storage/app/public/ogp/Microposts.png" />
+        <meta property="og:site_name" content="Microposts" />
+        <meta property="og:description" content="フレームワークLaravelではじめてのWebサービス作りをしました。" />
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
+    
     <body>
         @include('commons.navbar')
 
